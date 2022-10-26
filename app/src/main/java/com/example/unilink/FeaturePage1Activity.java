346a71d4b0f33c984bf2d093daf1d312e6ed3b92
ruland39 <rulandfurqan39@gmail.com>
2022-10-26 12:41:13 +0800

    add FeaturePage1Activity.java

---
+++ app/src/main/java/com/example/unilink/FeaturePage1Activity.java
@@ -0,0 +1,14 @@
+package com.example.unilink;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FeaturePage1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.feature_page1);
+    }
+}
--- app/src/main/java/com/example/unilink/MainActivity.java
+++ app/src/main/java/com/example/unilink/MainActivity.java
@@ -4,11 +4,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.content.Intent;
+
+import android.os.Handler;
+import android.view.View;
+import android.view.WindowManager;
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //configuring window to full screen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        //set first page
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this, FeaturePage1Activity.class);
+
+                startActivity(i);
+
+                finish();
+            }
+        }, 2000);
     }
 }
