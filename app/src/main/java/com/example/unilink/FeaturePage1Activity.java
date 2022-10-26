35087b7ce6399126549ba88ec47b3fc19db4b284
ruland39 <rulandfurqan39@gmail.com>
2022-10-26 12:59:51 +0800

    add FeaturePage3Activity.java

--- app/src/main/java/com/example/unilink/FeaturePage1Activity.java
+++ app/src/main/java/com/example/unilink/FeaturePage1Activity.java
@@ -2,13 +2,28 @@ package com.example.unilink;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class FeaturePage1Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.feature_page1);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(FeaturePage1Activity.this, FeaturePage2Activity.class);
+
+                startActivity(i);
+
+                finish();
+            }
+        }, 1000);
     }
 }
---
+++ app/src/main/java/com/example/unilink/FeaturePage3Activity.java
@@ -0,0 +1,14 @@
+package com.example.unilink;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FeaturePage3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.feature_page3);
+    }
+}
