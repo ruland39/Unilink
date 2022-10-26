afb72c83253f7db20bd183a392ba48985199147f
ruland39 <rulandfurqan39@gmail.com>
2022-10-26 17:25:08 +0800

    added LoginorregisterActivity

--- app/src/main/java/com/example/unilink/FeaturePage3Activity.java
+++ app/src/main/java/com/example/unilink/FeaturePage3Activity.java
@@ -2,13 +2,28 @@ package com.example.unilink;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FeaturePage3Activity extends AppCompatActivity {
-
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.feature_page3);
+
+        button = findViewById(R.id.getstartedbutton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLoginorRegisterPage();
+            }
+        });
+    }
+    public void openLoginorRegisterPage(){
+        Intent intent = new Intent(this, LoginorregisterActivity.class);
+        startActivity(intent);
     }
 }
