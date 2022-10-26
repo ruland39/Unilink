ba6218474297880e0e71499bcb8ba8ced452f47f
ruland39 <rulandfurqan39@gmail.com>
2022-10-26 18:36:37 +0800

    login and register page done

--- app/src/main/java/com/example/unilink/LoginorregisterActivity.java
+++ app/src/main/java/com/example/unilink/LoginorregisterActivity.java
@@ -2,13 +2,44 @@ package com.example.unilink;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginorregisterActivity extends AppCompatActivity {
-
+    private Button button1; //login button
+    private Button button2; //register button
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.loginorregister);
+
+        button1 = (Button) findViewById(R.id.loginbutton);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLoginPage();
+            }
+        });
+
+        button2 = findViewById(R.id.registerbutton);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openRegisterPage();
+            }
+        });
     }
+
+    public void openLoginPage(){
+        Intent intent1 = new Intent(this, LoginpageActivity.class);
+        startActivity(intent1);
+    }
+
+    public void openRegisterPage(){
+        Intent intent2 = new Intent(this, RegisterpageActivity.class);
+        startActivity(intent2);
+    }
+
 }
