e1355211d5dcd73f58725ca7094560d933b38c22
ruland39 <rulandfurqan39@gmail.com>
2022-10-26 12:43:28 +0800

    add FeaturePage2Activity.java

---
+++ app/src/main/java/com/example/unilink/FeaturePage2Activity.java
@@ -0,0 +1,14 @@
+package com.example.unilink;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FeaturePage2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.feature_page2);
+    }
+}
