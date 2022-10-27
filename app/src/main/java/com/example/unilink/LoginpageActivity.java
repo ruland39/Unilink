b9e63402fbfdfdd0cb3ad150620f5c05ee7a8b97
ruland39 <rulandfurqan39@gmail.com>
2022-10-27 12:22:29 +0800

    add back button and minor changes

--- app/src/main/java/com/example/unilink/LoginpageActivity.java
+++ app/src/main/java/com/example/unilink/LoginpageActivity.java
@@ -2,13 +2,31 @@ package com.example.unilink;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class LoginpageActivity extends AppCompatActivity {
+    private ImageButton backbutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.loginpage);
+
+        backbutton = findViewById(R.id.backbutton);
+        backbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openBacktoLoginorRegisterPage();
+            }
+        });
+
+    }
+
+    public void openBacktoLoginorRegisterPage() {
+        Intent intent = new Intent(this, LoginorregisterActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/unilink/RegisterpageActivity.java
+++ app/src/main/java/com/example/unilink/RegisterpageActivity.java
@@ -2,13 +2,29 @@ package com.example.unilink;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class RegisterpageActivity extends AppCompatActivity {
-
+    private ImageButton backbutton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registerpage);
+
+        backbutton = findViewById(R.id.backbutton);
+        backbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openBacktoLoginorRegisterPage();
+            }
+        });
+    }
+    public void openBacktoLoginorRegisterPage(){
+        Intent intent = new Intent(this, LoginorregisterActivity.class);
+        startActivity(intent);
     }
 }
