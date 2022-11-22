ddcfea4ed27bc4795644c1c1df40d34a987af83e
xenonial98 <mirza898@gmail.com>
2022-11-23 00:56:42 +0800

    Loads without Feature Page when logged in

--- app/src/main/java/com/example/unilink/MainActivity.java
+++ app/src/main/java/com/example/unilink/MainActivity.java
@@ -3,39 +3,35 @@ package com.example.unilink;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
+import org.xml.sax.HandlerBase;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 import android.os.Bundle;
-
 import android.content.Intent;
-
+import android.content.SharedPreferences;
 import android.os.Handler;
 import android.view.View;
 import android.view.WindowManager;
 
-
 public class MainActivity extends AppCompatActivity {
-
+    private SharedPreferences sharedPref;
+    private FirebaseAuth mAuth;
 
     // onCreate refers to a method that fires when the app is *created*
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //forces light mode
+        // forces light mode
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
 
-        //configuring window to full screen
+        // configuring window to full screen
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        //set first page
+        // set first page
         setContentView(R.layout.activity_main);
-        // basically, hold the layout main page for 1.5 seconds
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent i = new Intent(MainActivity.this, FeaturePage1Activity.class);
-                startActivity(i);    
-                finish();        
-            }
-        }, 1500);
+        mAuth = FirebaseAuth.getInstance();
     }
 
     @Override
@@ -45,13 +41,38 @@ public class MainActivity extends AppCompatActivity {
     }
 
     // onStart method is called when the activity enters the Started state
-    protected void onStart(Bundle savedInstanceState) {
-
+    @Override
+    public void onStart() {
+        super.onStart();
+        // basically, hold the layout main page for 1.5 seconds
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                FirebaseUser currentUsr = mAuth.getCurrentUser();
+                // in session
+                if (currentUsr != null) {
+                    String userId = currentUsr.getUid();
+                    sharedPref = getPreferences(MODE_PRIVATE);
+                    SharedPreferences.Editor editor = sharedPref.edit();
+                    editor.putString("firebasekey", userId).commit();
+                    openHomeScreen();
+                    finish();
+                } else
+                    openFp();
+            }
+        }, 1500);
     }
 
-    // onResume method is called just before the activity starts an interaction with the user (button not pressable but rendered)
-    // most of an app's functionality should be here
-    protected void onResume(Bundle savedInstanceState) {
-
+    private void openFp() {
+        Intent i = new Intent(MainActivity.this, FeaturePage1Activity.class);
+        startActivity(i);
+        finish();
     }
+
+    private void openHomeScreen() {
+        Intent i = new Intent(MainActivity.this, HomescreenActivity.class);
+        startActivity(i);
+        finish();
+    }
+
 }
