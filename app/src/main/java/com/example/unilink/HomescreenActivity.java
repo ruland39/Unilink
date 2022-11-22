637304f3559432dcb01c2dd1396f2e671253a417
ruland39 <hcyrf1@nottingham.edu.my>
2022-11-22 09:23:08 +0800

    implemented navigation drawer and its functionalities

--- app/src/main/java/com/example/unilink/HomescreenActivity.java
+++ app/src/main/java/com/example/unilink/HomescreenActivity.java
@@ -1,7 +1,11 @@
 package com.example.unilink;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -21,16 +25,22 @@ import android.view.View;
 import android.widget.ImageButton;
 
 import com.example.unilink.databinding.ActivityMainBinding;
+
+import org.checkerframework.checker.units.qual.A;
 import org.checkerframework.checker.units.qual.C;
 
 public class HomescreenActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     private SharedPreferences sharedPref;
-    //DrawerLayout drawerLayout;
-    //NavigationView navigationView;
+
     ActivityMainBinding binding;
     BottomNavigationView bottomNavigationView;
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
+
     HomeFragment homeFragment = new HomeFragment();
     ChatFragment chatFragment = new ChatFragment();
     NotificationFragment notificationFragment = new NotificationFragment();
@@ -54,41 +64,65 @@ public class HomescreenActivity extends AppCompatActivity {
         setContentView(R.layout.homescreen);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, homeFragment).commit();
-        
+
         mAuth = FirebaseAuth.getInstance();
 
-//     drawerLayout = findViewById(R.id.drawer_layout);
-//     navigationView = findViewById(R.id.collapseMenu);
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
         bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()){
-                    case R.id.home: getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, homeFragment).commit();
+                switch (item.getItemId()) {
+                    case R.id.home:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, homeFragment).commit();
                         return true;
-                    case R.id.chat: getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, chatFragment).commit();
+                    case R.id.chat:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, chatFragment).commit();
                         return true;
-                    case R.id.notification: getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, notificationFragment).commit();
+                    case R.id.notification:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, notificationFragment).commit();
                         return true;
-                    case R.id.profile: getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, profileFragment).commit();
+                    case R.id.profile:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, profileFragment).commit();
                         return true;
                 }
                 return false;
             }
         });
-        
-        // for now collapsemenu signs you out for authentication purposes
-        ImageButton collapsemenubtn = findViewById(R.id.collapseMenuButton);
-        collapsemenubtn.setOnClickListener(new View.OnClickListener() {
+
+
+        drawerLayout = findViewById(R.id.drawerLayout);
+        navigationView = findViewById(R.id.navigationView);
+        toolbar = findViewById(R.id.toolbar);
+
+
+        setSupportActionBar(toolbar);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.opendrawer, R.string.closedrawer);
+
+        drawerLayout.addDrawerListener(toggle);
+
+        toggle.syncState();
+
+        //to open the options in the drawe
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onClick(View v) {
-                openCollapseMenu();
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                // Settings
+                // Logout
+                // Support
+                // Terms and Policies
+                // About
+
+                drawerLayout.closeDrawer(GravityCompat.START);
+                return true;
             }
         });
-        
-         }
-         
-        public void openCollapseMenu() {
+
+    }
+
+        //logout method
+        public void logout() {
         // drawerLayout.setVisibility(View.VISIBLE);
         // remove sharedpreferences
         // open loginorregister
@@ -101,112 +135,25 @@ public class HomescreenActivity extends AppCompatActivity {
         finish();
     }
 
+        //load fragment method
+        public void loadfragment(){
+            FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.add(R.id.frame_layout, homeFragment);
+            ft.commit();
+        }
+
+        //back method
+        public void onBackPressed (){
+            if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+                drawerLayout.closeDrawer(GravityCompat.START);
+            }
+            else{
+                super.onBackPressed();
+            }
+
+        }
+
+
 }
-
-//        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomnavigationview);
-//        bottomNavigationView.setBackground(null);
-//        getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, new HomeFragment()).commit();
-//        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-//            @Override
-//            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-//                Fragment temp = null;
-//
-//                switch (item.getItemId()) {
-//
-//                    case R.id.homeBtn:
-//                        temp = new HomeFragment();
-//                        break;
-//
-//                    case R.id.chatBtn:
-//                        temp = new ChatFragment();
-//                        break;
-//
-//                    case R.id.notifBtn:
-//                        temp = new NotificationFragment();
-//                        break;
-//
-//                    case R.id.profileBtn:
-//                        temp = new ProfileFragment();
-//                        break;
-//                }
-//
-//                getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, temp).commit();
-//                return true;
-//
-//            }
-//
-//
-//        });
-
-
-//        ImageButton collapsemenubtn = findViewById(R.id.collapseMenuButton);
-//        collapsemenubtn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                //drawerLayout.openDrawer(GravityCompat.START);
-//                openCollapseMenu();
-//            }
-//        });
-//        ImageButton homebtn = findViewById(R.id.homeBtn);
-//        homebtn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                openHomeScreen();
-//
-//            }
-//        });
-//
-//        ImageButton chatbtn = findViewById(R.id.chatBtn);
-//        chatbtn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                openChatScreen();
-//
-//            }
-//        });
-//
-//        ImageButton notifbtn = findViewById(R.id.notifBtn);
-//        notifbtn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                openNotifScreen();
-//
-//            }
-//        });
-//
-//        ImageButton profilebtn = findViewById(R.id.profileBtn);
-//        profilebtn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                openProfileScreen();
-//
-//            }
-//        });
-
-
-
-//    public void openHomeScreen(){
-//        Intent i = new Intent(this, HomescreenActivity.class);
-//        startActivity(i);
-//    }
-//
-//    public void openChatScreen(){
-//        Intent i = new Intent(this, ChatscreenActivity.class);
-//        startActivity(i);
-//    }
-//
-//    public void openNotifScreen(){
-//        Intent i = new Intent(this, NotificationscreenActivity.class);
-//        startActivity(i);
-//    }
-//
-//    public void openProfileScreen(){
-//        Intent i = new Intent(this, ProfilescreenActivity.class);
-//        startActivity(i);
-//    }
-//
-//    public void openCollapseMenu(){
-//        drawerLayout.setVisibility(View.VISIBLE);
-//    }
-//}
 
