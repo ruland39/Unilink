60c37ea89d0162b85ee35158aea961684f24c041
xenonial98 <mirza898@gmail.com>
2022-11-28 16:47:43 +0800

    Retrieve user information in Main when logged in

--- app/src/main/java/com/example/unilink/MainActivity.java
+++ app/src/main/java/com/example/unilink/MainActivity.java
@@ -2,22 +2,34 @@ package com.example.unilink;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.annotation.NonNull;
 
 import org.xml.sax.HandlerBase;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 import android.os.Bundle;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Handler;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Toast;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
-    private SharedPreferences sharedPref;
+import com.example.unilink.UnilinkUser;
+import com.google.gson.Gson;
+
+public class MainActivity extends AppCompatActivity {    
     private FirebaseAuth mAuth;
+    private FirebaseFirestore db;
 
     // onCreate refers to a method that fires when the app is *created*
     @Override
@@ -32,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
         // set first page
         setContentView(R.layout.activity_main);
         mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
     }
 
     @Override
@@ -52,15 +65,45 @@ public class MainActivity extends AppCompatActivity {
                 // in session
                 if (currentUsr != null) {
                     String userId = currentUsr.getUid();
-                    sharedPref = getPreferences(MODE_PRIVATE);
-                    SharedPreferences.Editor editor = sharedPref.edit();
-                    editor.putString("firebasekey", userId).commit();
+                    getPreferences(MODE_PRIVATE).edit().putString("firebasekey", userId).commit();
+                    getUserInfo(currentUsr);
                     openHomeScreen();
                 } else
                     openFp();
             }
         }, 1500);
     }
+
+        // Get the user information from firestore 
+        private void getUserInfo(FirebaseUser user) {
+            Log.d("com.example.unilink", "Getting User Information from Firestore for: " + user.getEmail());
+            db.collection("user_information")
+                    .whereEqualTo("authId", user.getUid())
+                    .get()
+                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>(){
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                            if (task.isSuccessful()) {
+                                for (QueryDocumentSnapshot doc : task.getResult()) {
+                                     UnilinkUser uUser = doc.toObject(UnilinkUser.class);
+                                     saveUserInfo(uUser);
+                                }
+                            } else {
+                                Log.w("com.example.unilink", "Error getting document: ", task.getException());
+                                Toast.makeText(getApplicationContext(), "Unable to get User Information", Toast.LENGTH_SHORT);                            
+                                finish();
+                            }
+                        }
+                    });
+        }
+    
+        // Save the user info into sharedpreference json
+        private void saveUserInfo(UnilinkUser user) {
+            Gson gson = new Gson();
+            String objString = gson.toJson(user);
+            getPreferences(MODE_PRIVATE).edit().putString("userJson", objString).commit();
+            Log.d("com.example.unilink", "Succesfully added User JSON to SharedPref: " + objString);
+        }
 
     private void openFp() {
         Intent i = new Intent(MainActivity.this, FeaturePage1Activity.class);
