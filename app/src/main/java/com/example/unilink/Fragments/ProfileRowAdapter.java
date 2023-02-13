e86e1a3d35a02bae278048494afe5b961c365f87
ruland39 <hcyrf1@nottingham.edu.my>
2023-02-13 17:25:55 +0800

    finished profilerowadapter home fragment

---
+++ app/src/main/java/com/example/unilink/Fragments/ProfileRowAdapter.java
@@ -0,0 +1,51 @@
+package com.example.unilink.Fragments;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.unilink.R;
+
+public class ProfileRowAdapter extends RecyclerView.Adapter<ProfileRowAdapter.ViewHolder> {
+    private String[] mDataset;
+    public ProfileRowAdapter(String[] dataset) {
+        mDataset = dataset;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.profile_row, parent, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+//        holder.getText().setText(mDataset[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataset.length;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+        private final CardView cardView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            cardView = (CardView) itemView.findViewById(R.id.profile_row_cardview);
+        }
+
+        public TextView getText() {
+            return (TextView) cardView.getChildAt(1);
+        }
+
+    }
+}
