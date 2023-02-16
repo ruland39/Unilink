34279a62789ddcf6d3f7e0297a70a8aa5df63345
ruland39 <hcyrf1@nottingham.edu.my>
2023-02-16 14:15:46 +0800

    fix notification row alignment

--- app/src/main/java/com/example/unilink/Fragments/NotificationFragment.java
+++ app/src/main/java/com/example/unilink/Fragments/NotificationFragment.java
@@ -23,7 +23,7 @@ public class NotificationFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-    private static final int DATASET_COUNT = 10;
+    private static final int DATASET_COUNT = 20;
 
     // TODO: Rename and change types of parameters
     private RecyclerView mRecyclerView;
