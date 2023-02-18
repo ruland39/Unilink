69661fb007b1cbb6a7456eb82aef6fb4ba4ee2be
xenonial98 <mirza898@gmail.com>
2023-02-19 00:47:33 +0800

    Fixed type error

--- app/src/main/java/com/example/unilink/Fragments/ProfileFragment.java
+++ app/src/main/java/com/example/unilink/Fragments/ProfileFragment.java
@@ -10,6 +10,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.PopupMenu;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -83,7 +84,7 @@ public class ProfileFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
 
-        Button threedotsbutton = view.findViewById(R.id.threedotsbutton);
+        ImageButton threedotsbutton = view.findViewById(R.id.threedotsbutton);
         threedotsbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
