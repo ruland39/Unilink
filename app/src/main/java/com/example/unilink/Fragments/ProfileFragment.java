d049ce0fea28833207f947152b61949f15b35cb2
ruland39 <hcyrf1@nottingham.edu.my>
2023-02-22 17:09:37 +0800

    added change profile picture & profile banner

--- app/src/main/java/com/example/unilink/Fragments/ProfileFragment.java
+++ app/src/main/java/com/example/unilink/Fragments/ProfileFragment.java
@@ -1,22 +1,37 @@
 package com.example.unilink.Fragments;
 
+import static android.app.Activity.RESULT_OK;
+
+import android.content.Intent;
+import android.media.Image;
+import android.nfc.Tag;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.MenuRes;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.PopupMenu;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.unilink.Models.UnilinkUser;
 import com.example.unilink.R;
+
+import java.sql.SQLOutput;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -30,12 +45,19 @@ public class ProfileFragment extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
+    private static final String TAG = "hello world";
+
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
     private UnilinkUser user;
     private static final String user_key = "user";
+    private static final int GALLERY_REQUEST_CODE = 1000;
+
+    ImageView profilePicture, profileBanner, temp;
+
+
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -65,16 +87,17 @@ public class ProfileFragment extends Fragment {
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
+
+
+
+
         }
 
-
-
-
-        // Bundle bundle = getArguments();        
+        // Bundle bundle = getArguments();
         // if(bundle != null) {
         //     user =  bundle.getParcelable("user");
         //     Log.d()
-        // } else 
+        // } else
         //     Toast.makeText(getActivity(), "Unable to parce User", Toast.LENGTH_SHORT).show();
     }
 
@@ -85,6 +108,13 @@ public class ProfileFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
 
         ImageButton threedotsbutton = view.findViewById(R.id.threedotsbutton);
+
+        // Images
+        profilePicture = view.findViewById(R.id.defaultprofilepicture);
+        profileBanner = view.findViewById(R.id.profilebanner);
+
+        // TextView
+
         threedotsbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -109,7 +139,25 @@ public class ProfileFragment extends Fragment {
         popup.getMenuInflater().inflate(menuRes, popup.getMenu());
 
         popup.setOnMenuItemClickListener(menuItem -> {
-            Toast.makeText(getActivity(), "LOl", Toast.LENGTH_SHORT).show();
+
+            switch (menuItem.getItemId()){
+
+                case R.id.editprofilepicture:
+                    Toast.makeText(getActivity(), "Choose Profile Picture", Toast.LENGTH_SHORT).show();
+                    pickImage(profilePicture);
+
+                    return true;
+
+                case R.id.editprofilebanner:
+                    Toast.makeText(getActivity(), "Choose Profile Banner (preferably wide image)", Toast.LENGTH_SHORT).show();
+                    pickImage(profileBanner);
+                    return true;
+
+                case R.id.editprofiledetails:
+                    Toast.makeText(getActivity(), "Profile Details", Toast.LENGTH_SHORT).show();
+                    return true;
+
+            }
             // Respond to menu item click.
             return true;
         });
@@ -125,4 +173,35 @@ public class ProfileFragment extends Fragment {
         popup.show();
     }
 
+    public ImageView pickImage(ImageView i){
+        temp = i;
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, GALLERY_REQUEST_CODE);
+        return temp;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == RESULT_OK){
+            if(requestCode == GALLERY_REQUEST_CODE){
+                temp.setImageURI(data.getData());
+            }
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+
+    }
+
+    @Override
+    public void onViewStateRestored(@Nullable Bundle savedInstanceState) {
+        super.onViewStateRestored(savedInstanceState);
+        Log.i(TAG, "On Restore Instance State");
+    }
 }
