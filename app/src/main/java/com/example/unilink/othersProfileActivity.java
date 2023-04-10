fdfe4c4145c0c5bdccfa6d4c8dc585b7265b9b20
ruland39 <hcyrf1@nottingham.edu.my>
2023-04-10 13:46:36 +0800

    added bottom sheet dialog friend request

--- app/src/main/java/com/example/unilink/othersProfileActivity.java
+++ app/src/main/java/com/example/unilink/othersProfileActivity.java
@@ -6,8 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.example.unilink.Activities.HomescreenActivity;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 public class othersProfileActivity extends AppCompatActivity {
 
@@ -15,6 +18,7 @@ public class othersProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_others_profile);
+        showBottomSheetDialog();
 
 
         ImageButton backbutton = findViewById(R.id.backbutton);
@@ -33,4 +37,32 @@ public class othersProfileActivity extends AppCompatActivity {
         startActivity(i);
         finish();
     }
+
+    public void showBottomSheetDialog() {
+        final BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(this);
+        bottomSheetDialog.setContentView(R.layout.bottom_sheet_dialog_friend_request);
+
+        LinearLayout accept = bottomSheetDialog.findViewById(R.id.acceptbutton);
+        LinearLayout ignore = bottomSheetDialog.findViewById(R.id.ignorebutton);
+
+        bottomSheetDialog.show();
+        
+        accept.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(othersProfileActivity.this, "Friend Request Accepted", Toast.LENGTH_SHORT).show();
+                bottomSheetDialog.dismiss();
+            }
+        });
+        
+        ignore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(othersProfileActivity.this, "Friend Request Ignored", Toast.LENGTH_SHORT).show();
+                bottomSheetDialog.dismiss();
+            }
+        });
+
+    }
 }
+
